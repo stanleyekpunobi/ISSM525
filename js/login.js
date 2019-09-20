d0196b80de2e904987fb42aa9cd01db042831113
Fathima Shakoora Farouk <shaksfarouk@gmail.com>
2019-09-19 18:56:49 -0600

    added js file for login page

---
+++ js/login.js
@@ -0,0 +1,30 @@
+ // JavaScript function when Login button is clicked
+ var loginform = document.loginform
+ var username = document.querySelector('#txtusername')
+ var password = document.querySelector('#txtpword')
+ var userNameArr = ["root", "admin", "comp"];
+ var passArr =["toor","123","qwerty"];
+
+ loginform.addEventListener('submit', function (e) {
+     //prevents form from re-loading
+     e.preventDefault();
+     
+         var isLoginValid =checkCred();
+         if(isLoginValid){
+             var newloc = location.href.substr(location.href.lastIndexOf('/'));
+             location.href = location.href.replace(newloc, '/store.html');
+             alert("Correct Credetials Entered!");
+         }else{
+             alert("Username or Password incorrect!");
+             document.getElementById("loginform").reset();
+         }        
+ })
+
+ function checkCred(){
+     for(i=0; i<= userNameArr.length; i++){
+         if (username.value == userNameArr[i] && password.value == passArr[i]) {
+             return true;
+         } 
+     }
+     return false;
+ }
